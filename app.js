7fcaeb862410af9f4efc163930e8cb2be22c86f5
Anwesh43 <anweshthecool0@gmail.com>
2017-01-04 01:33:02 +0530

    Server changes

--- app.js
+++ app.js
@@ -6,14 +6,16 @@ var server = http.createServer(app)
 app.use(express.static(path.join(__dirname,'public')))
 var socketIO = require('socket.io')
 var io = socketIO(server)
-
+var sockets = []
 io.of('game').on('connection',(socket)=>{
+    sockets.push(socket)
     console.log('a client is connected')
     console.log(socket)
     socket.emit("hello",{msg:"thanks for connection"})
     socket.on('SEND_GAME_OBJECT',(gameObjectJson)=>{
         console.log(gameObjectJson)
-        
+        var id = socket.id
+        sockets.filter((curr_socket)=>curr_socket.id!=id).forEach((curr_socket)=>{curr_socket.emit('RECEIVE_GAME_OBJECT',gameObjectJson)})
     })
 
 }) //Creating a namespace
